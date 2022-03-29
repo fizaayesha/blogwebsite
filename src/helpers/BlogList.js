98c6d208b54980ce37c62227e3ab02152fcc7b7f
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2022-03-30 00:30:41 +1200

    Update BlogList.js

--- src/helpers/BlogList.js
+++ src/helpers/BlogList.js
@@ -1,6 +1,5 @@
 import image1 from "../assets/image1.jpeg";
 import image2 from "../assets/image2.jpeg";
-import image3 from "../assets/image3.jpeg";
 export const BlogList = [
   {
     name: "Women in Tech",
